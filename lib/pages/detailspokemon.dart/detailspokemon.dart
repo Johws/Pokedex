901946c727caec1262543f49dd70dd5a34b26f80
Jonatas <joba.herc@gmail.com>
2023-11-01 15:22:26 -0300

    implemetacao details

--- lib/pages/detailspokemon.dart/detailspokemon.dart
+++ lib/pages/detailspokemon.dart/detailspokemon.dart
@@ -9,43 +9,78 @@ class PokemonDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text(
-          'Pokemon Details',
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Text(
+            'Pokemon Details',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListTile(
-              leading: Image.network(
-                pokemon.thumbnailImage ?? '',
-                width: 100,
-                height: 100,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(20), // Adiciona margens ao redor do conteúdo
+            padding: EdgeInsets.all(
+                30), // Adiciona preenchimento interno ao conteúdo
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 2.0,
               ),
-              title: Text(
-                '${pokemon.name ?? ''} - ${pokemon.number ?? ''}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 20,
+                ), // Adiciona um espaço em cima da imagem
+                Center(
+                  child: Image.network(
+                    pokemon.thumbnailImage ?? '',
+                    width:
+                        200, // Ajusta o tamanho da imagem conforme necessário
+                    height:
+                        200, // Ajusta o tamanho da imagem conforme necessário
+                  ),
                 ),
-              ),
+                ListTile(
+                  title: Text(
+                    '${pokemon.name ?? ''} - ${pokemon.number ?? ''}',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+
+                ListTile(
+                  title: Text(
+                    'Detalhes: ${pokemon.description ?? ''}',
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Altura: ${pokemon.height ?? ''}',
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Habilidade: ${pokemon.abilities ?? ''}',
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    'Tipo: ${pokemon.type ?? ''}',
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+              ],
             ),
-            ListTile(
-              title: Text(
-                'Height: ${pokemon.height ?? ''}',
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Abilities: ${pokemon.abilities?.join(", ") ?? ''}',
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
